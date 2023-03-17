f56258eaa99e71696962df8f3aa9af5b10cfaed9
Vertical <ambintasso@gmail.com>
2023-03-17 12:47:26 +0530

    components

---
+++ src/Cards.js
@@ -0,0 +1,12 @@
+function Cards() {
+  return (
+    <div className="cards">
+      <h3>img</h3>
+      <h3>img2</h3>
+      <h3>img3</h3>
+      <h3>img4</h3>
+    </div>
+  );
+}
+
+export default Cards;
---
+++ src/Score.js
@@ -0,0 +1,9 @@
+function Score() {
+  return (
+    <div className="score">
+      <h2>Score: 10</h2>
+      <h2>Best: 50</h2>
+    </div>
+  );
+}
+export default Score;
