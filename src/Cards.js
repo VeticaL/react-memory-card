9b03705d8d29f50e36f483352a62a04bb0dfd813
Vertical <ambintasso@gmail.com>
2023-03-22 13:00:05 +0530

    images

--- src/Cards.js
+++ src/Cards.js
@@ -5,10 +5,6 @@ function Cards() {
       <img src={`https://robohash.org/diablo`}  alt="robots"/>
       <img src={`https://robohash.org/pes`}  alt="robots"/>
       <img src={`https://robohash.org/car`}  alt="robots"/>
-      {/* <h3>img</h3>
-      <h3>img2</h3>
-      <h3>img3</h3>
-      <h3>img4</h3> */}
     </div>
   );
 }
